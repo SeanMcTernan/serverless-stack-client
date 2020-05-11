703d1b26c3b717651329b140a989cef5545945c8
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-05-11 17:05:02 -0600

    Seting up our React App

---
+++ config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-bts"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://1xr9awgez4.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_g8n0kbh5F",
+      APP_CLIENT_ID: "8028p812oltheg5s4bhghk09u",
+      IDENTITY_POOL_ID: "us-east-1:2c1b9aff-9c6e-4e8e-83be-10a6860c0400"
+    }
+  };
